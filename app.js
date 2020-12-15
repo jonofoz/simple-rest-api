4181de1c5ff462cb08e50c66746b5978c7df92eb
JonOfOz <jon@jonofoz.com>
2020-12-15 10:56:43 -0600

    Implemented /list endpoint to fetch all records

--- app.js
+++ app.js
@@ -1,40 +1,54 @@
 const express = require('express');
+const { MongoClient } = require('mongodb');
 const app = express();
 const router = express.Router()
 
 const PORT = process.env.PORT      || 5000;
+const URI  = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017';
+
+const client = MongoClient(URI, {useUnifiedTopology: true});
 
 app.use(express.json());
 
+(async () => await client.connect())();
+
 router.get('/list', async (req, res) => {
-    // TODO: DB
-    const records = [];
+    const db = client.db('default');
+    const collection = db.collection('starterData');
+    // Store cursor for all records in the DB
+    const cursor = await collection.find({});
+    const records = await cursor.toArray()
     const numberOfRecords = records.length
-    res.json({ message: `${numberOfRecords} records fetched successfully.`, records });
+    console.log(`${numberOfRecords} records fetched successfully.`)
+    res.json(records);
 })
 router.post('/create', async (req, res) => {
     // TODO: DB
     const record = req.body;
+    console.log('Record created successfully.')
     // Node's default response.statusCode is 200, so we specify others like 201 as needed.
-    res.status(201).json({ message: `Record created successfully.`, record });
+    res.status(201).json(record);
 })
 router.get('/read/:recordId', async (req, res) => {
     // TODO: DB
     const recordId = req.params.recordId;
     const record = {};
-    res.json({ message: `Record ${recordId} fetched successfully.`, record });
+    console.log(`Record ${recordId} fetched successfully.`)
+    res.json(record);
 })
 router.put('/modify/:recordId', async (req, res) => {
     // TODO: DB
     const recordId = req.params.recordId;
     const record = {};
-    res.json({ message: `Record ${recordId} updated successfully.`, record });
+    console.log(`Record ${recordId} updated successfully.`)
+    res.json(record);
 })
 router.delete('/remove/:recordId', async (req, res) => {
     // TODO: DB
     const recordId = req.params.recordId;
     const record = {};
-    res.json({ message: `Record ${recordId} deleted successfully.`, record });
+    console.log(`Record ${recordId} deleted successfully.`)
+    res.json(record);
 })
 
 app.use('/api', router);
