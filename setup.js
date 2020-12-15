7a9271b6f5915027552add23ab35f98071a1bb3b
JonOfOz <jon@jonofoz.com>
2020-12-14 21:25:15 -0600

    Create setup for starterData on a MongoDB backend

---
+++ setup.js
@@ -0,0 +1,53 @@
+require('dotenv').config()
+const { ObjectID } = require('mongodb');
+const { MongoClient } = require('mongodb');
+
+const starterData = require('./starterData');
+const URI = process.env.MONGO_URI || process.env.MONGO_URI_DEFAULT;
+
+const client = MongoClient(URI, {useUnifiedTopology: true});
+
+
+async function disconnectFromDB(exitStatus=null) {
+    try {
+        await client.close();
+    }
+    finally {
+        if (exitStatus == 1) {
+            process.exit(exitStatus);
+        }
+    }
+}
+
+/**
+ * This function places  basic starter data into the database for the REST API to work with. (Generated with Mockaroo.)
+ */
+async function setupStarterData() {
+    try {
+        await client.connect();
+        const db = client.db('default');
+        const collection = db.collection('starterData')
+        // Remove the starterData collection if it already exists
+        await collection.deleteMany({})
+        // Populate the collection with the starterData
+        for await (let doc of starterData) {
+            try {
+                doc.creationDate = doc.lastModificationDate = new Date().getTime();
+                await collection.insertOne(doc);
+            }
+            catch (err) {
+                console.log(err)
+            }
+        };
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
+
+setupStarterData()
+    .then(() => {
+        console.log("Starter data successfully added to the database!")
+    })
+    .finally(() => disconnectFromDB())
+    .catch(err => console.log(err.stack))
---
+++ starterData.js
@@ -0,0 +1,51 @@
+module.exports = [{
+    "timestamp": 1585717200000,
+    "value1": "Garek Dadge",
+    "value2": 82.6,
+    "value3": false
+  }, {
+    "timestamp": 1591851600000,
+    "value1": "Onfre Fynn",
+    "value2": 21.7,
+    "value3": true
+  }, {
+    "timestamp": 1605852000000,
+    "value1": "Daren Shillam",
+    "value2": 62.1,
+    "value3": true
+  }, {
+    "timestamp": 1602133200000,
+    "value1": "Farra Sayburn",
+    "value2": 96.2,
+    "value3": false
+  }, {
+    "timestamp": 1593666000000,
+    "value1": "Arni Versey",
+    "value2": 76.8,
+    "value3": true
+  }, {
+    "timestamp": 1577944800000,
+    "value1": "Conchita Dawtry",
+    "value2": 27.2,
+    "value3": true
+  }, {
+    "timestamp": 1599022800000,
+    "value1": "Lelah Drew-Clifton",
+    "value2": 87.5,
+    "value3": false
+  }, {
+    "timestamp": 1603688400000,
+    "value1": "Read Faulkes",
+    "value2": 23.0,
+    "value3": false
+  }, {
+    "timestamp": 1578031200000,
+    "value1": "Lowe Pannaman",
+    "value2": 46.3,
+    "value3": false
+  }, {
+    "timestamp": 1600232400000,
+    "value1": "Tamarah Winny",
+    "value2": 7.2,
+    "value3": false
+  }]
