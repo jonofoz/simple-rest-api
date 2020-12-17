625a3a4ce2f281b61753b8452cda4322f54bf889
JonOfOz <jon@jonofoz.com>
2020-12-17 15:04:52 -0600

    Put API routes into separate file, general tidying up:
      With the documentation, I decided the app.js file was getting too big. The API now lives in ./routes/api.js.
      I've also cleaned up a lot of unused code.

---
+++ routes/api.js
@@ -0,0 +1,306 @@
+const express = require('express');
+const { ObjectID } = require('mongodb');
+const APIrouter = express.Router();
+
+// Swagger - documentation setup
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
+
+const options = {
+    definition: {
+      openapi: "3.0.0",
+      info: {
+        title: "A Simple REST API",
+        description: "This is a simple REST API!",
+        version: "3.0.0"
+        },
+        externalDocs: {
+            description: "Documentation built with Swagger.",
+            url: "http://swagger.io"
+        },
+        // TODO: Determine which servers are available to execute API calls on, depending on where the documentation is hosted.
+      servers: [
+        { url: "https://simple-rest-api-jonofoz.herokuapp.com/" },
+        { url: "http://simple-rest-api-jonofoz.herokuapp.com/" },
+        { url: "http://localhost:5000/" }
+      ],
+    },
+    apis: ["./routes/api.js"],
+};
+
+const specs = swaggerJsdoc(options);
+APIrouter.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(specs)
+);
+
+// Swagger - components setup
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Record:
+ *       type: object
+ *       properties:
+ *         _id:
+ *           description: The 12 byte, hexademical representation of a MongoDB ObjectId
+ *           type: string
+ *           format: byte
+ *           example: 5fdb9473354a9f3b6cf7cdee
+ *         timestamp:
+ *           description: The unix time, in milliseconds, that the record was created
+ *           type: integer
+ *           example: 1578031200000
+ *         value1:
+ *           type: string
+ *           example: Lowe Pannaman
+ *         value2:
+ *           type: number
+ *           format: float
+ *           example: 46.3
+ *         value3:
+ *           type: boolean
+ *           example: false
+ *         lastModificationDate:
+ *           description: The unix timestamp, in milliseconds, of the last time the record was modified
+ *           type: integer
+ *           example: 1608225907502
+ *         creationDate:
+ *           description: The unix timestamp, in milliseconds, that the record was first saved to the database
+ *           type: integer
+ *           example: 1608225907502
+ */
+
+/**
+ * @swagger
+ * /api/list:
+ *     get:
+ *       tags:
+ *       - API
+ *       summary: Fetch all records from the database
+ *       description: Record data will be returned as an array of JSON objects.
+ *       responses:
+ *         "200":
+ *           description: All records fetched successfully
+ */
+APIrouter.get('/list', async (req, res, next) => {
+    try {
+        const allRecords = await req.collection.find({}).toArray();
+        console.log(`${allRecords.length} records fetched successfully.`);
+        res.json(allRecords);
+    }
+    catch (err) {
+        next(err);
+    }
+})
+
+/**
+ * @swagger
+ * /api/read/{recordId}:
+ *   get:
+ *     tags:
+ *     - API
+ *     summary: Fetch a record from the database
+ *     description: Record will be returned as a JSON object. See example below.
+ *     parameters:
+ *     - name: recordId
+ *       in: path
+ *       description: The 12 byte, hexademical representation of a MongoDB ObjectId
+ *       example: 5fdb9473354a9f3b6cf7cdee
+ *       required: true
+ *       schema:
+ *         type: string
+ *         format: byte
+ *     responses:
+ *       "200":
+ *         description: Record was fetched successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Record'
+ *       "400":
+ *         description: ID supplied was not valid
+ *       "404":
+ *         description: Record with that ID wasn't found
+ */
+APIrouter.get('/read/:recordId', async (req, res, next) => {
+    try {
+        const recordId = req.params.recordId;
+        const record = await req.collection.findOne({ _id: ObjectID(recordId) });
+        if (record == null) {
+            throw new Error(`No record with ID ${recordId} was found.`);
+        }
+        console.log(`Record ${recordId} fetched successfully.`);
+        res.json(record);
+    }
+    catch (err) {
+        next(err);
+    }
+})
+
+/**
+ * @swagger
+ * /api/create:
+ *   post:
+ *     tags:
+ *     - API
+ *     summary: Add a new record to the database
+ *     requestBody:
+ *       description: Any combination of the fields in an existing record can be modified. If the field did not already exist in the record, it will be added.
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Record'
+ *     responses:
+ *       "200":
+ *         description: Record was created successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Record'
+ *             example:
+ *               timestamp: 1500434700000
+ *               value1: "Vashti Bunyan"
+ *               value2: 26.4
+ *               value3: true
+ *       "400":
+ *         description: ID supplied was not valid
+ *       "404":
+ *         description: Record with that ID wasn't found
+ */
+APIrouter.post('/create', async (req, res, next) => {
+    try {
+        const record = req.body;
+        record.creationDate = record.lastModificationDate = new Date().getTime();
+        await req.collection.insertOne(record);
+        console.log('Record created successfully.');
+        res.status(201).json(record);
+    }
+    catch (err) {
+        next(err);
+    }
+})
+
+/**
+ * @swagger
+ * /api/modify/{recordId}:
+ *   put:
+ *     tags:
+ *     - API
+ *     summary: Update a record in the database
+ *     parameters:
+ *     - name: recordId
+ *       in: path
+ *       description: The 12 byte, hexademical representation of a MongoDB ObjectId
+ *       example: 5fdb9473354a9f3b6cf7cdec
+ *       required: true
+ *       schema:
+ *         type: string
+ *         format: byte
+ *     requestBody:
+ *       description: Any combination of the fields in an existing record can be modified. If the field did not already exist in the record, it will be added.
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Record'
+ *           example:
+ *             value1: "J. R. R. Tolkien"
+ *             value2: 33.333
+ *             value3: true
+ *     responses:
+ *       "200":
+ *         description: Record was modified successfully and the new version was returned
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Record'
+ *       "400":
+ *         description: Supplied ID wasn't valid
+ *       "404":
+ *         description: No record with that ID was found
+ */
+APIrouter.put('/modify/:recordId', async (req, res, next) => {
+    try {
+        const recordId = req.params.recordId;
+        const fieldsToUpdate = req.body;
+        fieldsToUpdate.lastModificationDate = new Date().getTime();
+        const record = await req.collection.findOneAndUpdate(
+            { _id: ObjectID(recordId) },
+            { $set: fieldsToUpdate },
+            { returnOriginal: false }
+        ).then(result => result.value)
+        if (record == null) {
+            throw new Error(`No record with ID ${recordId} was found.`);
+        }
+        console.log(`Record ${recordId} updated successfully.`);
+        res.json(record);
+    }
+    catch (err) {
+        next(err);
+    }
+})
+
+/**
+ * @swagger
+ * /api/remove/{recordId}:
+ *   delete:
+ *     tags:
+ *     - API
+ *     summary: Delete record in the database
+ *     parameters:
+ *     - name: recordId
+ *       in: path
+ *       description: The 12 byte, hexademical representation of a MongoDB ObjectId
+ *       example: 5fdb9473354a9f3b6cf7cdea
+ *       required: true
+ *       schema:
+ *         type: string
+ *         format: byte
+ *     responses:
+ *       "200":
+ *         description: Record was deleted successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Record'
+ *       "400":
+ *         description: ID supplied was not valid
+ *       "404":
+ *         description: Record with that ID wasn't found
+ */
+APIrouter.delete('/remove/:recordId', async (req, res, next) => {
+    try {
+        const recordId = req.params.recordId;
+        debugger;
+        const deletedRecord = await req.collection.findOneAndDelete({ _id: ObjectID(recordId) }).then((result) => result.value);
+        if (deletedRecord == null) {
+            throw new Error(`No record with ID ${recordId} was found.`);
+        }
+        console.log(`Record deleted successfully. (ID: ${recordId})`);
+        res.json(deletedRecord);
+    }
+    catch (err) {
+        next(err);
+    }
+})
+
+// Error handling middleware
+APIrouter.use((err, req, res, next) => {
+
+    // The errors thrown by the MongoDB wrapper do not appear to include status codes.
+    // Choose and send the appropriate code here, plus the included error message.
+    const message = err.message;
+    var status = 500;
+    if (message.includes("Argument passed in must be a single String of 12 bytes or a string of 24 hex characters")) {
+        status = 400;
+    }
+    else if (message.includes("No record with ID")) {
+        status = 404;
+    }
+    res.status(status).send({ err: err.message });
+    next();
+})
+
+module.exports = APIrouter;
