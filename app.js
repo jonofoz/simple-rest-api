bcbb8c6dabf33f71126d837f387595751d014677
JonOfOz <jon@jonofoz.com>
2020-12-18 15:34:18 -0600

    Implement tests scaffolded in README.md:
      This is, unfortunately, a rather large commit. :/
      I'm not completely satisfied with the results yet: I don't like calling
      Jest using --detectOpenHandles and --force-exit, but its my quick fix
      for when Jest tests don't properly finish. I think I know what the problem
      is, and it's related to not properly disconnecting from MongoDB. I just
      haven't found the a graceful solution yet, if that's the problem.

--- app.js
+++ app.js
@@ -6,17 +6,23 @@ const app = express();
 
 app.use(express.json());
 app.use(require("cors")({optionsSuccessStatus: 200}));
-app.set('port', process.env.PORT || 5000);
 
-
+const testing = process.env.NODE_ENV === "test";
 // Connect to MongoDB
+const PORT = testing ? 3333 : process.env.PORT;
+const DB_NAME         = testing ? process.env.DB_NAME_TEST         : process.env.DB_NAME_PRODUCTION;
+const COLLECTION_NAME = testing ? process.env.COLLECTION_NAME_TEST : process.env.COLLECTION_NAME_PRODUCTION;
 const URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017';
+
+app.set('port', PORT || 5000);
+
 var collection;
-MongoClient.connect(URI, { useUnifiedTopology: true }, (err, client) => {
+const connection = MongoClient.connect(URI, { useUnifiedTopology: true }, (err, client) => {
     if (err) {
         throw err;
     }
-    collection = client.db('default').collection('starterData');
+    collection = client.db(DB_NAME).collection(COLLECTION_NAME);
+
 })
 
 app.use((req, res, next) => {
@@ -27,6 +33,16 @@ app.use((req, res, next) => {
 
 app.use('/api', require('./routes/api'));
 
-app.listen(app.get('port'), () => {
-    console.log(`App listening on port ${app.get('port')}!`)
-});
+/*
+    SuperTest handles the opening and closing of a test instance of app.
+    We don't call listen() manually in test: we could potentially create
+    orphan processes if we don't close them before tests finish.
+*/
+
+if (!testing) {
+    app.listen(app.get('port'), () => {
+        console.log(`App listening on port ${app.get('port')}!`)
+    });
+}
+
+module.exports = app;
--- tests/api.test.js
+++ tests/api.test.js
@@ -1,3 +1,218 @@
-test('Test 1', () => {
-    expect(1).toBe(1);
+require('dotenv').config()
+const baseURL = process.env.BASE_URL || 'http://127.0.0.1:5000';
+const request = require('supertest');
+
+const { populateDBWithStarterData, clearDBData } = require('../utilities');
+
+const URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017'
+
+const DB_NAME = process.env.DB_NAME_TEST;
+const COLLECTION_NAME = process.env.COLLECTION_NAME_TEST;
+
+var app;
+
+beforeAll(() => {
+    const baseURL = process.env.BASE_URL || 'http://127.0.0.1:5000';
+    app = require('../app');
 })
+
+beforeEach(async () => {
+    await populateDBWithStarterData(URI, DB_NAME, COLLECTION_NAME)
+        .then(() => console.log("Test data successfully added to the database!"))
+        .catch(err => console.log(err.stack))
+})
+afterEach(async () => {
+    await clearDBData(URI, DB_NAME, COLLECTION_NAME)
+        .then(() => console.log("Test data successfully cleared from the database!"))
+        .catch(err => console.log(err.stack))
+})
+
+// This helper function pings the API for the current number of records in a database's collection.
+async function getNumberOfRecords() {
+    return await request(app)
+        .get('/api/recordsCount')
+        .expect(200)
+        .then(res => res.body);
+}
+
+describe("GET / ", () => {
+    test('Should list all records in the database', async () => {
+        // There should 10 records returned
+        expect.assertions(1);
+        const records = await request(app)
+            .get('/api/list')
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .then(res => res.body);
+        expect(records.length).toBe(10);
+    })
+
+    test('Should fetch a record from the database', async () => {
+        // Should get Lowe Pannaman and any expected values
+        expect.assertions(4);
+        const record = await request(app)
+            .get('/api/read/999999999999999999999999')
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .then(res => res.body);
+        expect(record.timestamp).toEqual(1578031200000);
+        expect(record.value1).toEqual('Lowe Pannaman');
+        expect(record.value2).toEqual(46.3);
+        expect(record.value3).toEqual(false);
+    })
+
+    test('Should fetch the number of records in the database', async () => {
+        // Should return 10
+        expect.assertions(1);
+        expect(await getNumberOfRecords()).toEqual(10);
+    })
+})
+
+describe('POST / ', () => {
+    test('Should create a new record in the database', async () => {
+        /*
+            Before creation:
+                There should be 10 records
+
+            After creation:
+                Record should have any expected values
+                There should be 11 records
+                Record should not have been in the database before creation
+        */
+        expect.assertions(7);
+
+        // Before creation:
+        const recordsOld = await request(app)
+            .get('/api/list')
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .then(res => res.body);
+        const recordsOldLength = recordsOld.length;
+        expect(recordsOldLength).toEqual(10);
+
+        // On creation:
+        const requestBody = {
+            'timestamp': 707,
+            'value1': 'Test McGee',
+            'value2': 5.5,
+            'value3': true
+        };
+        const newRecord = await request(app)
+            .post('/api/create')
+            .send(requestBody)
+            .set('Accept', 'application/json')
+            .expect(201)
+            .expect('Content-Type', /json/)
+            .then(res => res.body)
+
+        // After creation:
+        expect(newRecord.timestamp).toEqual(requestBody['timestamp']);
+        expect(newRecord.value1).toEqual(requestBody['value1']);
+        expect(newRecord.value2).toEqual(requestBody['value2']);
+        expect(newRecord.value3).toEqual(requestBody['value3']);
+
+        // There should be one more record now
+        expect(await getNumberOfRecords()).toEqual(recordsOldLength + 1);
+        // Here we check if, for whatever reason, the new record was already there
+        const newRecordWasAlreadyThere = recordsOld.filter(rec => rec._id === newRecord._id).length > 0;
+        expect(newRecordWasAlreadyThere).toBeFalsy();
+    })
+})
+
+describe('PUT / ', () => {
+    test('Should modify a record in the database', async () => {
+        /*
+            Before modification:
+                Record should have any expected values
+                There should be 10 records
+
+            After modification:
+                Record should have any expected values
+                There should still be 10 records
+        */
+
+        expect.assertions(10);
+
+        // Before modification:
+
+        // Let's modify Lowe Pannaman
+        const record = await request(app)
+            .get('/api/read/999999999999999999999999')
+            .expect(200)
+            .then(res => res.body);
+        expect(record.timestamp).toEqual(1578031200000);
+        expect(record.value1).toEqual('Lowe Pannaman');
+        expect(record.value2).toEqual(46.3);
+        expect(record.value3).toEqual(false);
+
+        expect(await getNumberOfRecords()).toEqual(10);
+
+        // On modification:
+        const requestBody = {
+            'timestamp': 1000,
+            'value1': 'Lower Panama',
+            'value2': 10.10,
+            'value3': true
+        };
+        const modifiedRecord = await request(app)
+            .put('/api/modify/999999999999999999999999')
+            .send(requestBody)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .then(res => res.body)
+
+        // After modification:
+        expect(modifiedRecord.timestamp).toEqual(requestBody['timestamp']);
+        expect(modifiedRecord.value1).toEqual(requestBody['value1']);
+        expect(modifiedRecord.value2).toEqual(requestBody['value2']);
+        expect(modifiedRecord.value3).toEqual(requestBody['value3']);
+
+        expect(await getNumberOfRecords()).toEqual(10);
+    })
+})
+
+describe('DELETE / ', () => {
+    test('Should remove a record from the database', async () => {
+        /*
+            Before removal:
+                There should be 10 records
+
+            After removal:
+                Record should have any expected values
+                There should be 9 records
+                Record should not be in the database after removal
+        */
+        expect.assertions(7);
+
+        // Before removal:
+        const numberOfRecordsOld = await getNumberOfRecords();
+        expect(numberOfRecordsOld).toEqual(10);
+
+        // On removal:
+        // Sorry Lowe, you gotta go
+        const deletedRecord = await request(app)
+            .delete('/api/remove/999999999999999999999999')
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .then(res => res.body)
+
+        // After removal:
+        expect(deletedRecord.timestamp).toEqual(1578031200000);
+        expect(deletedRecord.value1).toEqual('Lowe Pannaman');
+        expect(deletedRecord.value2).toEqual(46.3);
+        expect(deletedRecord.value3).toEqual(false);
+        // There should be one less record now
+        const recordsNew = await request(app)
+        .get('/api/list')
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .then(res => res.body);
+        expect(recordsNew.length).toEqual(numberOfRecordsOld - 1);
+
+        // Here we check if, for whatever reason, the deleted record is still there
+        const deletedRecordIstStillThere = recordsNew.filter(rec => rec._id === deletedRecord._id).length > 0;
+        expect(deletedRecordIstStillThere).toBeFalsy();
+    })
+})
+
